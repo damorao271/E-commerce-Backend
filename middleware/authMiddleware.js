2d8c06dd98498e97ba83224c13c68abd29537502
Daniel Morao <13-10932@usb.ve>
2020-06-21 03:59:41 -0400

    Auth done

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -5,14 +5,17 @@ const myToken = process.env.MYTOKEN;
 
 function auth(req, res, next) {
   const token = req.header("x-auth-token");
+  console.log("token", token);
   if (!token) res.status(401).send("Access denied. No token provided");
 
   try {
     const decoded = jwt.verify(token, myToken);
+
     req.user = decoded;
+
     next();
   } catch (err) {
-    res.status(400).send("Invalid Token");
+    res.status(400).send("Invalid Token !");
   }
 }
 
