17b9ebd30d73888655eebdefcb76c2158cdda2e1
Daniel Morao <13-10932@usb.ve>
2020-06-21 02:08:27 -0400

    WIthout auth and admin

--- routes/products.js
+++ routes/products.js
@@ -74,7 +74,7 @@ router.get("/name/:id", validateObjectId, async (req, res) => {
   res.send(product);
 });
 
-router.post("/", [auth, admin], async (req, res) => {
+router.post("/", async (req, res) => {
   const { error } = validate(req.body);
   if (error) return res.status(400).send(error.details[0].message);
 
